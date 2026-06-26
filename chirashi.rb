6ae2901c29df20124b0b4d61d7c7210eaa6baeca
G <g@devnull.rip>
2026-06-26 02:54:53 -0700

    chirashi v0.5.0: pure Go, no REX SDK, darwin per-arch

--- chirashi.rb
+++ chirashi.rb
@@ -3,42 +3,32 @@ class Chirashi < Formula
   homepage 'https://github.com/g-lok/chirashi'
   license 'MIT'
 
-  version 'v0.3.1'
+  version 'v0.5.0'
 
   on_macos do
-    url "https://github.com/g-lok/chirashi/releases/download/#{version}/chirashi-#{version}-macos.tar.gz"
-    sha256 'e32f281b84dc83ee462de620bec782af0889b246ba5823497ef61cd0565906d6'
+    on_intel do
+      url "https://github.com/g-lok/chirashi/releases/download/#{version}/chirashi-#{version}-darwin-amd64.tar.gz"
+      sha256 '416b9ce712c084739b3b4682b18145b1a862dc5c702678aa07cab040151877b6'
+    end
+    on_arm do
+      url "https://github.com/g-lok/chirashi/releases/download/#{version}/chirashi-#{version}-darwin-arm64.tar.gz"
+      sha256 '0a0a84d42dbec6d75b6da16eb97c489e2b4ead80c5cda76036c55de09800ede5'
+    end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/g-lok/chirashi/releases/download/#{version}/chirashi-#{version}-linux-amd64"
-      sha256 'e362802dc4deb0bc99e02962f2cb3767ea4176b4f7183d8af4e46cf231edc910'
+      url "https://github.com/g-lok/chirashi/releases/download/#{version}/chirashi-#{version}-linux-amd64.tar.gz"
+      sha256 '293b495c633da85624f6e6e64ab6b382672b9043967d3cb68ed8639dacbd05ac'
     end
     on_arm do
-      url "https://github.com/g-lok/chirashi/releases/download/#{version}/chirashi-#{version}-linux-arm64"
-      sha256 '220373094aff24904df467ba76831c871127db00148f0e4f7662897be10762a8'
+      url "https://github.com/g-lok/chirashi/releases/download/#{version}/chirashi-#{version}-linux-arm64.tar.gz"
+      sha256 '32edf3bca9823340cb29fec6cbe1386fc8889f7f828e8f15311e879b8b7d5d68'
     end
   end
 
   def install
     bin.install 'chirashi'
-    on_macos do
-      frameworks.install 'Frameworks/REX Shared Library.framework'
-    end
-  end
-
-  def caveats
-    on_macos do
-      <<~EOS
-        chirashi includes the REX Shared Library framework for REX/RX2/RCY
-        input. The framework is installed to:
-          #{frameworks}/REX Shared Library.framework
-
-        On Linux, REX input is not available (the REX SDK is macOS/Windows
-        only). All other formats work cross-platform.
-      EOS
-    end
   end
 
   test do
